8361b82629b6446618fc381179d16e2a1c6fad29
victorVaflo <victorvaflo@gmail.com>
2020-11-22 02:58:49 -0500

    gulp con watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,17 @@ const ts = require('gulp-typescript')
 const project = ts.createProject('tsconfig.json')
 const pug = require('gulp-pug')
 
-// gulp.task('pug', function () {
-//     return gulp.src('src/*.pug').pipe(gulp.dest('dist'));
-// })
+gulp.task('pug', function () {
+    return gulp.src('src/*.pug').pipe(pug({})).pipe(gulp.dest('dist'));
+})
 
-// gulp.task('compileTs', function () {
-//     return gulp.src('src/*.ts').pipe(gulp.dest('dist'));
-// })
+gulp.task('compileTs', function () {
+    return gulp.src('src/*.ts').pipe(project()).pipe(gulp.dest('dist'));
+})
 
-// gulp.task('watchTs', function () {
-//     gulp.watch('src/*.ts', ['compileTs']);
-// });
-
-// gulp.task('watchPug', function () {
-//     gulp.watch('src/*.pug', ['pug']);
-// });
 
 gulp.task('build', function (done) {
-    gulp.src('src/*.ts').pipe(project()).pipe(gulp.dest('dist'));
-    gulp.src('src/*.pug').pipe(pug({})).pipe(gulp.dest('dist'));
+    gulp.watch('src/*.ts', series('compileTs'));
+    gulp.watch('src/*.pug', series('pug'));
     done();
 });
